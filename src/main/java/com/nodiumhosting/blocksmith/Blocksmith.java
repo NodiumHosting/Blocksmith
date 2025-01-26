f8e16de85b1b7cc397ae28f5a4671ffa9086bdc1
McMelonTV <mcmelontube@gmail.com>
2025-01-26 17:02:29 +0100

    remove main method

--- src/main/java/com/nodiumhosting/blocksmith/Blocksmith.java
+++ src/main/java/com/nodiumhosting/blocksmith/Blocksmith.java
@@ -1,8 +1,4 @@
 package com.nodiumhosting.blocksmith;
 
 public class Blocksmith {
-    public static void main(String[] args) {
-        System.out.println("You can only use blocksmith as a library.");
-        System.exit(1);
-    }
 }
