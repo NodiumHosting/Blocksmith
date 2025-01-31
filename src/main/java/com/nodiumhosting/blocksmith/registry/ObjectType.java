c6938053fd87781556e890492d1cc18e84aebfb3
McMelonTV <mcmelontube@gmail.com>
2025-01-31 02:14:40 +0100

    make ObjectType abstract

--- src/main/java/com/nodiumhosting/blocksmith/registry/ObjectType.java
+++ src/main/java/com/nodiumhosting/blocksmith/registry/ObjectType.java
@@ -2,7 +2,7 @@ package com.nodiumhosting.blocksmith.registry;
 
 import com.nodiumhosting.blocksmith.common.Identifier;
 
-public class ObjectType<TypeData extends ObjectTypeData> {
+public abstract class ObjectType<TypeData extends ObjectTypeData> {
     public final Identifier id;
     private final TypeData data;
 
